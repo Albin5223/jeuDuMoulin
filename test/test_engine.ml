91a66b9b71c306b1f6f86cdc0f55331d138135e7
paris <paris.albin23@gmail.com>
2023-11-15 11:25:34 +0100

    Unused function

--- test/test_engine.ml
+++ test/test_engine.ml
@@ -21,20 +21,7 @@ let test_place_piece =
           let board, _ = place_piece_on_board board coord color in
           get_square board coord = Some (Color color)
         else true)
-
-let list_of_square_empty (board : board) : coordinates list =
-    let rec aux board i j =
-        if i = List.length board && j = List.length board
-        then []
-        else if j = List.length board
-        then aux board (i + 1) 0
-        else
-          let square = get_square board (i, j) in
-          if square = Some Empty then (i, j) :: aux board i (j + 1) else aux board i (j + 1)
-    in
-    aux board 0 0
     
-
 let () =
     let open Alcotest in
     run "TEST ENGINE" [("Test place piece", [QCheck_alcotest.to_alcotest test_place_piece])]
